ef27a1caa836621ba05b65f7752a459198feb5e6
klausmayr <55473722+klausmayr@users.noreply.github.com>
2022-11-04 12:03:18 -0400

    Update isochrone.py

--- isochrone.py
+++ isochrone.py
@@ -3,7 +3,7 @@ import json
 import geojson
 
 start = 'https://api.mapbox.com/isochrone/v1/mapbox/driving/'
-end = '?contours_minutes=5&contours_colors=6706ce&polygons=true&access_token=pk.eyJ1Ijoia2xhdXNtYXlyIiwiYSI6ImNsM20xZWFxejAwNmkza29mcHptN243aTYifQ.lfLe-OcdZ89adHFcD7r9tQ'
+end = '?contours_minutes=5&contours_colors=6706ce&polygons=true&access_token=[YOUR_MAPBOX_API_KEY]'
 
 with open('galveston_2022_polling_places.geojson') as f:
     gj = geojson.load(f)
